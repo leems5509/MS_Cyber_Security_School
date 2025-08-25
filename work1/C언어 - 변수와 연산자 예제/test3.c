bbc21d413dffc7f4d8c5c11299a20c5e079f7a8d
leems5509 <ruelsszz@gmail.com>
2025-08-25 16:27:10 +0900

    Add files via upload

---
+++ work1/C언어 - 변수와 연산자 예제/test3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	int x,y;
+	int A,O,X,N;
+	printf("x y A O X N\n");
+	printf("============\n");
+	x=1,y=1; 
+	printf("%d %d %d %d %d %d\n",x,y,x&&y,x||y,x^y,!x);
+	 
+	x=1,y=0;
+	printf("%d %d %d %d %d %d\n",x,y,x&&y,x||y,x^y,!x);
+	
+	x=0,y=1;
+	printf("%d %d %d %d %d %d\n",x,y,x&&y,x||y,x^y,!x);
+	
+	x=0,y=0;
+	printf("%d %d %d %d %d %d\n",x,y,x&&y,x||y,x^y,!x);
+}
---
+++ work1/C언어 - 변수와 연산자 예제/test4.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	int n=5.25; //소수부의 손실
+	double d=3; //값의 표현이 넓은 범위로의 변환 
+	int c=129;
+	
+	printf("n:%d  d:%1f  c:%d\n", n, d, c); 
+}
